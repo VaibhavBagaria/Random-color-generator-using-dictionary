0aae5f2d0336aa09b0d478b9cff8d30f1efb4ac8
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2022-01-05 18:49:57 -0300

    Add files via upload

---
+++ Random Colour Generator using Dictionary.py
@@ -0,0 +1,40 @@
+from tkinter import *
+import random
+root=Tk()
+root.title("Random Color Generator using Dictionary")
+root.geometry('400x400')
+
+label_color=Label(root)
+#109_Number of colors below
+Random_color = ['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white', 'old lace',
+          'linen', 'antique white', 'papaya whip', 'blanched almond', 'bisque', 'peach puff',
+          'navajo white', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
+          'lavender blush', 'misty rose', 'dark slate gray', 'dim gray', 'slate gray',
+          'light slate gray', 'gray', 'light gray', 'midnight blue', 'navy', 'cornflower blue', 'dark slate blue',
+          'slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue',  'blue',
+          'dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+          'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise',
+          'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green',
+          'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green', 'spring green',
+          'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green',
+          'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow',
+          'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown',
+          'indian red', 'saddle brown', 'sandy brown',
+          'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange',
+          'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink',
+          'pale violet red', 'maroon', 'medium violet red', 'violet red',
+          'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
+          'thistle', 'snow2', 'snow3']
+Dict={"Color":Random_color}
+
+def bg_color():
+    NG=random.randint(0, 108)
+    Get_Color=Dict["Color"][NG]
+    root.configure(background=Get_Color)
+    label_color['text']=Get_Color
+
+button1=Button(root,text="Change Color", command=bg_color)
+label_color.place(relx=0.5,rely=0.5,anchor=CENTER)
+button1.place(relx=0.5,rely=0.6,anchor=CENTER)
+
+root.mainloop()
